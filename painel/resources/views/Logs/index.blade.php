8d8417a42462bf1c331c3952707641358e4b4617
Luiz Fernando <luizfernando.itpro@gmail.com>
2026-05-04 12:18:26 +0100

    fix: changes

--- painel/resources/views/Logs/index.blade.php
+++ painel/resources/views/Logs/index.blade.php
@@ -1,6 +1,20 @@
-@extends('layouts.master')
-@section('content')
-<div class="page-header"><h3 class="page-title"> Logs </h3><nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item"><a href="{{url('')}}/">Dashboard</a></li><li class="breadcrumb-item active" aria-current="page">Logs</li></ol></nav></div>
-<div class="row"><div class="col-lg-12 grid-margin stretch-card"><div class="card"><div class="card-body"><div class="table-responsive">@if (empty($viewModel->objectReturn) || count($viewModel->objectReturn) === 0) @include('shared/empty-table') @else @include('Logs/list-table') @endif</div></div></div></div></div>
-@include('shared/pagination')
-@endsection
+@extends('layouts.master') @section('content') <div class="page-header">
+  <h3 class="page-title"> Logs </h3>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a href="{{url('')}}/">Dashboard</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page">Logs</li>
+    </ol>
+  </nav>
+</div>
+<div class="row">
+  <div class="col-lg-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <div class="table-responsive">@if (empty($viewModel->objectReturn) || count($viewModel->objectReturn) === 0) @include('shared/empty-table') @else @include('Logs/list-table') @endif</div>
+      </div>
+    </div>
+  </div>
+</div> @include('shared/pagination') @endsection
--- painel/resources/views/Logs/list-table.blade.php
+++ painel/resources/views/Logs/list-table.blade.php
@@ -1,1 +1,39 @@
-<table class="table table-striped"><thead><tr><th>ID</th><th>Nome</th><th>Mensagem</th><th>Tipo</th><th>Data</th></tr></thead><tbody>@foreach ($viewModel->objectReturn as $Log)<tr><td>{{$Log->id}}</td><td>{{$Log->name}}</td><td>{{$Log->message}}</td><td>{{$Log->type}}</td><td>{{$Log->created_at}}</td></tr>@endforeach</tbody></table>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Nome</th>
+      <th>Mensagem</th>
+      <th>Tipo</th>
+      <th>Data</th>
+    </tr>
+  </thead>
+  <tbody>@foreach ($viewModel->objectReturn as $Log) <tr>
+      <td>{{$Log->id}}</td>
+      <td>{{$Log->name}}</td>
+      <td>{{$Log->message}}</td>
+      <td>
+        @switch($Log->type)
+            @case(\App\Enums\LogType::Error)
+                <span class="badge badge-danger">Error</span>
+                @break
+
+            @case(\App\Enums\LogType::Warn)
+                <span class="badge badge-warning">Warning</span>
+                @break
+
+            @case(\App\Enums\LogType::Info)
+                <span class="badge badge-info">Info</span>
+                @break
+
+            @case(\App\Enums\LogType::Fatal)
+                <span class="badge badge-error">Fatal</span>
+                @break
+
+            @default
+                <span class="badge badge-secondary">Unknown</span>
+        @endswitch
+      </td>
+      <td>{{$Log->created_at}}</td>
+    </tr>@endforeach </tbody>
+</table>
